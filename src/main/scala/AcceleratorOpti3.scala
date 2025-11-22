6136f7f511fb11b2707fe9c61c5f61876391d311
TristanBALDIT <tristan.baldit@gmail.com>
2025-11-22 23:45:58 +0100

    Working version (570 cycles) without full boundary writing

--- src/main/scala/AcceleratorOpti3.scala
+++ src/main/scala/AcceleratorOpti3.scala
@@ -1,7 +1,7 @@
 import chisel3._
 import chisel3.util._
 
-class AcceleratorOpti3 {
+class AcceleratorOpti3 extends Module {
   val io = IO(new Bundle {
     val start = Input(Bool())
     val done = Output(Bool())
@@ -13,27 +13,52 @@ class AcceleratorOpti3 {
 
   })
 
+  val idle :: readCenter :: readInnerCross :: readOuterCross :: writeCross :: done :: Nil = Enum(6)
+
   val state = RegInit(idle)
 
   val cross_center_x = RegInit(0.U(6.W))
   val cross_center_y = RegInit(0.U(6.W))
+  val initial_x = RegInit(0.U(6.W))
+  val initial_y = RegInit(0.U(6.W))
 
   val center_address = cross_center_x * 20.U + cross_center_y
 
+  val write_idx = RegInit(0.U(4.W))
+  val read_idx = RegInit(0.U(4.W))
+
+  val previous_cross_buffers =  RegInit(VecInit(Seq.fill(2)(VecInit(Seq.fill(2)(1.U(8.W))))))
+  val next_cross_buffers = RegInit(VecInit(Seq.fill(2)(1.U(8.W))))
+
+  val actual_cross = RegInit(0.U(1.W))
+  val previous_cross = !actual_cross
+
+  val bot_diag_buffers = RegInit(VecInit(Seq.fill(2)(VecInit(Seq.fill(10)(VecInit(Seq.fill(2)(1.U(8.W))))))))
+  val top_actual_diag_buffer = RegInit(VecInit(Seq.fill(10)(VecInit(Seq.fill(2)(1.U(8.W))))))
+
+  val actual_line = RegInit(0.U(1.W))
+  val previous_line = !actual_line
+
+  val buffer_idx = RegInit(0.U(6.W))
+  val initial_buffer_idx = RegInit(8.U(6.W))
+
+  val cross_buffer = RegInit(VecInit(Seq.fill(5)(0.U(8.W))))
+
+
   val cross_addresses = VecInit(Seq(
-    cross_center_x * 20.U + cross_center_y,                 // center
-    (cross_center_x + 1.U) * 20.U + cross_center_y,         // bottom
-    (cross_center_x - 1.U) * 20.U + cross_center_y,         // top
-    cross_center_x  * 20.U + (cross_center_y + 1.U),        // right
-    cross_center_x  * 20.U + (cross_center_y - 1.U),        // left
-    (cross_center_x + 1.U) * 20.U + (cross_center_y - 1.U), // bottom left
-    (cross_center_x - 1.U) * 20.U + (cross_center_y - 1.U), // top left
-    (cross_center_x - 1.U) * 20.U + (cross_center_y + 1.U), // top right
-    (cross_center_x + 1.U) * 20.U + (cross_center_y + 1.U), // bottom right
-    (cross_center_x + 2.U) * 20.U + cross_center_y,         // bottom bottom
-    cross_center_x * 20.U + (cross_center_y - 2.U),         // left left
-    (cross_center_x - 2.U) * 20.U + cross_center_y,         // top top
-    cross_center_x * 20.U + (cross_center_y + 2.U)          // right right
+    cross_center_y * 20.U + cross_center_x,                 // center
+    (cross_center_y + 1.U) * 20.U + cross_center_x,         // bottom
+    (cross_center_y - 1.U) * 20.U + cross_center_x,         // top
+    cross_center_y  * 20.U + (cross_center_x + 1.U),        // right
+    cross_center_y  * 20.U + (cross_center_x - 1.U),        // left
+    (cross_center_y + 1.U) * 20.U + (cross_center_x - 1.U), // bottom left
+    (cross_center_y - 1.U) * 20.U + (cross_center_x - 1.U), // top left
+    (cross_center_y - 1.U) * 20.U + (cross_center_x + 1.U), // top right
+    (cross_center_y + 1.U) * 20.U + (cross_center_x + 1.U), // bottom right
+    (cross_center_y + 2.U) * 20.U + cross_center_x,         // bottom bottom
+    cross_center_y * 20.U + (cross_center_x - 2.U),         // left left
+    (cross_center_y - 2.U) * 20.U + cross_center_x,         // top top
+    cross_center_y * 20.U + (cross_center_x + 2.U)          // right right
   ))
 
   val cross_addresses_valid = VecInit(Seq(
@@ -53,45 +78,22 @@ class AcceleratorOpti3 {
   ))
 
   val outer_usefull = VecInit(Seq(
-    cross_buffer(1) === 255.U || cross_buffer(4) === 255.U,
-    cross_buffer(2) === 255.U || cross_buffer(4) === 255.U,
-    cross_buffer(2) === 255.U || cross_buffer(3) === 255.U,
-    cross_buffer(1) === 255.U || cross_buffer(3) === 255.U,
-    cross_buffer(1) === 255.U,
-    cross_buffer(4) === 255.U,
-    cross_buffer(2) === 255.U,
-    cross_buffer(3) === 255.U
+    cross_buffer(1) === 255.U || cross_buffer(4) === 255.U || (io.dataRead(7,0) === 255.U && read_idx === 4.U),
+    (cross_buffer(2) === 255.U || (cross_buffer(4) === 255.U && !(io.dataRead(7,0) === 0.U && read_idx === 5.U)) || (io.dataRead(7,0) === 255.U && read_idx === 4.U)) && previous_cross_buffers(previous_cross)(0) === 1.U ,
+    ((cross_buffer(2) === 255.U && !(io.dataRead(7,0) === 0.U && read_idx === 6.U)) || cross_buffer(3) === 255.U) && bot_diag_buffers(previous_line)(buffer_idx)(1) === 1.U,
+    (cross_buffer(1) === 255.U && !(io.dataRead(7,0) === 0.U && read_idx === 7.U)) || (cross_buffer(3) === 255.U && !(io.dataRead(7,0) === 0.U && read_idx === 7.U)),
+    cross_buffer(1) === 255.U && !(io.dataRead(7,0) === 0.U && (read_idx === 5.U || read_idx === 8.U)),
+    cross_buffer(4) === 255.U && previous_cross_buffers(previous_cross)(1) === 1.U && !(io.dataRead(5,0) === 0.U && (read_idx === 6.U || read_idx === 6.U)),
+    cross_buffer(2) === 255.U && bot_diag_buffers(previous_line)(buffer_idx)(0) === 1.U && !(io.dataRead(7,0) === 0.U && (read_idx === 6.U || read_idx === 7.U)),
+    cross_buffer(3) === 255.U && top_actual_diag_buffer(buffer_idx + 1.U)(0) === 1.U && !(io.dataRead(7,0) === 0.U && (read_idx === 7.U || read_idx === 8.U))
   ))
-
-  val write_idx = RegInit(0.U(4.W))
-  val read_idx = RegInit(0.U(4.W))
-  val readOuter_idx = RegInit(0.U(4.W))
-  val idx = RegInit(0.U(4.W))
-
-  val idle :: readCenter :: readInnerCross :: readOuterCross :: writeCross :: Nil = Enum(5)
-
-  val previous_cross_buffers =  RegInit(VecInit(Seq.fill(2)(VecInit(Seq.fill(2)(0.U(8.W))))))
-
-  val actual_cross = RegInit(0.U(1.W))
-  val previous_cross = actual_cross + 1.U
-
-  val bot_diag_buffers = RegInit(VecInit(Seq.fill(2)(VecInit(Seq.fill(10)(VecInit(Seq.fill(2)(0.U(8.W))))))))
-  val top_actual_diag_buffer = RegInit(VecInit(Seq.fill(10)(VecInit(Seq.fill(2)(0.U(8.W))))))
-
-  val actual_line = RegInit(0.U(1.W))
-  val previous_line = actual_line + 1.U
-
-  val buffer_idx = RegInit(8.U(6.W))
-  val initial_buffer_idx = RegInit(8.U(6.W))
-
-  val cross_buffer = RegInit(VecInit(Seq.fill(5)(0.U(8.W))))
-
-  val addressWrite = cross_addresses(write_idx)
-  val addressRead = cross_addresses(read_idx)
 
   io.done := false.B
   io.writeEnable := false.B
-  io.address := Mux(io.writeEnable, addressWrite, addressRead)
+
+  val addressWrite = cross_addresses(write_idx)
+  val addressRead = cross_addresses(read_idx)
+  io.address := Mux(io.writeEnable, addressWrite + 400.U, addressRead)
 
   io.dataWrite := cross_buffer(write_idx)
 
@@ -103,9 +105,11 @@ class AcceleratorOpti3 {
 
         cross_center_x := 16.U
         cross_center_y := 0.U
+        initial_x := 16.U
+        initial_y := 0.U
 
-        buffer_idx := 8.U
-        initial_buffer_idx := 8.U
+        buffer_idx := 6.U
+        initial_buffer_idx := 6.U
 
         actual_line := 0.U
         actual_cross := 0.U
@@ -135,7 +139,7 @@ class AcceleratorOpti3 {
           read_idx := read_idx + 1.U
         // ELSE GO TOP
         }.otherwise {
-          read_idx := read_idx + 1.U
+          read_idx := read_idx + 2.U
         }
       }
     }
@@ -145,30 +149,52 @@ class AcceleratorOpti3 {
       io.writeEnable := true.B
 
       // EXIT STATE
-      when(write_idx === 5.U || (write_idx === 4.U && !cross_addresses_valid(write_idx+1.U))){
+      when(write_idx === 4.U || (write_idx === 3.U && !cross_addresses_valid(write_idx+1.U))){
         state := readCenter
         write_idx := 0.U
         //CONTINUE DIAGONAL
         when(cross_center_x  < 18.U && cross_center_y < 19.U){
           cross_center_x := cross_center_x + 2.U
-          cross_center_y := cross_center_y + 2.U
+          cross_center_y := cross_center_y + 1.U
+          buffer_idx := buffer_idx + 1.U
+          actual_cross := !actual_cross
 
         // NEW DIAGONAL
         }.otherwise{
+
+          //RESET NEXT BUFFER AND PREVIOUS
+          next_cross_buffers := VecInit(Seq.fill(2)(1.U(8.W)))
+          previous_cross_buffers := VecInit(Seq.fill(2)(VecInit(Seq.fill(2)(1.U(8.W)))))
+          actual_cross := 0.U
+
+          // UPDATE BOT LINE BUFFERS
+          bot_diag_buffers(previous_line) := VecInit(Seq.fill(10)(VecInit(Seq.fill(2)(1.U(8.W)))))
+          actual_line := previous_line
+
           // BIGGER DIAGONAL
-          when(cross_center_x - 5.U >= 0.U){
-            cross_center_x := cross_center_x - 5.U
-            buffer_idx := initial_buffer_idx - 1.U
+          when(initial_x >= 5.U){
+            initial_x := initial_x - 5.U
+            cross_center_x := initial_x - 5.U
+            cross_center_y := initial_y
+            buffer_idx := initial_buffer_idx - 2.U
             initial_buffer_idx := initial_buffer_idx - 2.U
           // SAME SIZE
-          }.elsewhen(cross_center_x - 1.U >= 0.U && cross_center_y + 2.U < 20.U){
-            cross_center_x := cross_center_x - 1.U
-            cross_center_y := cross_center_y + 2.U
+          }.elsewhen(initial_x  >= 1.U && initial_y + 2.U < 20.U){
+            initial_x := initial_x - 1.U
+            initial_y := initial_y + 2.U
+            cross_center_x := initial_x - 1.U
+            cross_center_y := initial_y + 2.U
+            buffer_idx := initial_buffer_idx
           // SMALLER DIAG
-          }.otherwise {
-            cross_center_x := cross_center_x + 1.U
-            cross_center_y := cross_center_y + 3.U
+          }.elsewhen(initial_y + 3.U < 20.U) {
+            initial_x := initial_x + 1.U
+            initial_y := initial_y + 3.U
+            cross_center_x := initial_x + 1.U
+            cross_center_y := initial_y + 3.U
             buffer_idx := initial_buffer_idx + 1.U
+            initial_buffer_idx := initial_buffer_idx + 1.U
+          }.otherwise {
+            state := done
           }
         }
 
@@ -184,7 +210,18 @@ class AcceleratorOpti3 {
 
     is(readInnerCross){
 
-      // TODO : ADD VALUE READ TO BOT DIAG BUFFER AND PREVIOUS CROSS BUFFER
+      switch(read_idx){
+        is(1.U){
+          bot_diag_buffers(actual_line)(buffer_idx)(1) := io.dataRead(7,0)
+          previous_cross_buffers(actual_cross)(1) := io.dataRead(7,0)
+        }
+        is(3.U){
+          previous_cross_buffers(actual_cross)(0) := io.dataRead(7,0)
+        }
+        is(4.U){
+          bot_diag_buffers(actual_line)(buffer_idx)(0) := io.dataRead(7,0)
+        }
+      }
 
       cross_buffer(read_idx) := io.dataRead(7,0)
 
@@ -210,43 +247,133 @@ class AcceleratorOpti3 {
           read_idx := 8.U
         }.otherwise {
           state := writeCross
+          read_idx := 0.U
         }
 
-      // WHEN NEXT IS TOP AND TOP ALREADY KNOWN : SKIP TOP
-      }.elsewhen(read_idx === 1.U && cross_addresses_valid(read_idx+1.U) && top_actual_diag_buffer(buffer_idx)(0) =/= 1.U){
+      // WHEN NEXT IS TOP AND TOP ALREADY KNOWN (FROM PREVIOUS LINE OR PREVIOUS CROSS): SKIP TOP
+      }.elsewhen(read_idx === 1.U && cross_addresses_valid(read_idx+1.U) && (top_actual_diag_buffer(buffer_idx)(0) =/= 1.U || next_cross_buffers(0) =/= 1.U)){
 
-        cross_buffer(read_idx + 1.U) := top_actual_diag_buffer(buffer_idx)(0)
+        when(top_actual_diag_buffer(buffer_idx)(0) =/= 1.U){
+          cross_buffer(read_idx + 1.U) := top_actual_diag_buffer(buffer_idx)(0)
+        }.otherwise {
+          cross_buffer(read_idx + 1.U) := next_cross_buffers(0)
+        }
 
         // SET CENTER TO BLACK IS BLACK LOADED
-        when(top_actual_diag_buffer(buffer_idx)(1) === 0.U){
+        when(top_actual_diag_buffer(buffer_idx)(1) === 0.U || next_cross_buffers(0) === 0.U){
           cross_buffer(0) := 0.U
         }
 
         // SKIP ALSO RIGHT
-        when(cross_addresses_valid(read_idx+1.U) && top_actual_diag_buffer(buffer_idx)(0) =/= 1.U){
+        when(cross_addresses_valid(read_idx+2.U) && top_actual_diag_buffer(buffer_idx)(0) =/= 1.U){
           cross_buffer(read_idx + 2.U) := top_actual_diag_buffer(buffer_idx)(1)
-          read_idx := read_idx + 3.U
-
+          previous_cross_buffers(actual_cross)(0) := top_actual_diag_buffer(buffer_idx)(1)
           // SET CENTER TO BLACK IS BLACK LOADED
           when(top_actual_diag_buffer(buffer_idx)(1) === 0.U){
             cross_buffer(0) := 0.U
           }
+
+          // SKIP ALSO LEFT
+          when(cross_addresses_valid(read_idx+3.U) && next_cross_buffers(1) =/= 1.U ){
+            cross_buffer(read_idx + 3.U)  := next_cross_buffers(1)
+            bot_diag_buffers(actual_line)(buffer_idx)(0) := next_cross_buffers(1)
+
+            when(next_cross_buffers(1) === 0.U){
+              cross_buffer(0) := 0.U
+            }
+
+            // EXIT
+            when(outer_usefull(0) && cross_addresses_valid(5)) {
+              state := readOuterCross
+              read_idx := 5.U
+            }.elsewhen(outer_usefull(1) && cross_addresses_valid(6)) {
+              state := readOuterCross
+              read_idx := 6.U
+            }.elsewhen(outer_usefull(2) && cross_addresses_valid(7)) {
+              state := readOuterCross
+              read_idx := 7.U
+            }.elsewhen(outer_usefull(3) && cross_addresses_valid(8)){
+              state := readOuterCross
+              read_idx := 8.U
+            }.otherwise {
+              state := writeCross
+            }
+
+          }.otherwise{
+            read_idx := read_idx + 3.U
+          }
         }.otherwise {
           read_idx := read_idx + 2.U
         }
-      // WHEN NEXT IS RIGHT AND RIGHT ALREADY KNOWN SKIP TOP
+      // WHEN NEXT IS RIGHT AND RIGHT ALREADY KNOWN SKIP RIGHT
       }.elsewhen(read_idx === 2.U && cross_addresses_valid(read_idx+1.U) && top_actual_diag_buffer(buffer_idx)(0) =/= 1.U){
-        read_idx := read_idx + 2.U
         cross_buffer(read_idx + 1.U) := top_actual_diag_buffer(buffer_idx)(1)
+        previous_cross_buffers(actual_cross)(0) := top_actual_diag_buffer(buffer_idx)(1)
 
         // SET CENTER TO BLACK IS BLACK LOADED
         when(top_actual_diag_buffer(buffer_idx)(1) === 0.U){
           cross_buffer(0) := 0.U
         }
+
+        // SKIP ALSO LEFT
+        when(cross_addresses_valid(read_idx+3.U) && next_cross_buffers(1) =/= 1.U ){
+          cross_buffer(read_idx + 3.U)  := next_cross_buffers(1)
+          bot_diag_buffers(actual_line)(buffer_idx)(0) := next_cross_buffers(1)
+
+          when(next_cross_buffers(1) === 0.U){
+            cross_buffer(0) := 0.U
+          }
+
+          // EXIT
+          when(outer_usefull(0) && cross_addresses_valid(5)) {
+            state := readOuterCross
+            read_idx := 5.U
+          }.elsewhen(outer_usefull(1) && cross_addresses_valid(6)) {
+            state := readOuterCross
+            read_idx := 6.U
+          }.elsewhen(outer_usefull(2) && cross_addresses_valid(7)) {
+            state := readOuterCross
+            read_idx := 7.U
+          }.elsewhen(outer_usefull(3) && cross_addresses_valid(8)){
+            state := readOuterCross
+            read_idx := 8.U
+          }.otherwise {
+            state := writeCross
+          }
+
+        }.otherwise{
+          read_idx := read_idx + 3.U
+        }
+
+      // WHEN NEXT IS LEFT AND LEFT ALREADY KNOWN SKIP LEFT
+      }.elsewhen(cross_addresses_valid(read_idx+3.U) && next_cross_buffers(1) =/= 1.U ){
+        cross_buffer(read_idx + 3.U)  := next_cross_buffers(1)
+        bot_diag_buffers(actual_line)(buffer_idx)(0) := next_cross_buffers(1)
+
+        when(next_cross_buffers(1) === 0.U){
+          cross_buffer(0) := 0.U
+        }
+
+        // EXIT
+        when(outer_usefull(0) && cross_addresses_valid(5)) {
+          state := readOuterCross
+          read_idx := 5.U
+        }.elsewhen(outer_usefull(1) && cross_addresses_valid(6)) {
+          state := readOuterCross
+          read_idx := 6.U
+        }.elsewhen(outer_usefull(2) && cross_addresses_valid(7)) {
+          state := readOuterCross
+          read_idx := 7.U
+        }.elsewhen(outer_usefull(3) && cross_addresses_valid(8)){
+          state := readOuterCross
+          read_idx := 8.U
+        }.otherwise {
+          state := writeCross
+        }
       // NEXT PIXEL OUTSIDE IMAGE : SKIP
-      }.elsewhen(!cross_addresses_valid(read_idx+1.U)) {
-        // NEXT NEWT PIXEL OUTSIDE IMAGE : SKIP
-        when(!cross_addresses_valid(read_idx+2.U)){
+      }.elsewhen(!cross_addresses_valid(read_idx+1.U) && read_idx < 3.U) {
+        // NEXT NEW PIXEL OUTSIDE IMAGE : SKIP
+        when(!cross_addresses_valid(read_idx+2.U) && read_idx < 2.U){
           read_idx := read_idx+3.U
         }
         read_idx := read_idx+2.U
@@ -257,8 +384,24 @@ class AcceleratorOpti3 {
 
     is(readOuterCross){
 
-      // TODO : ADD VALUE READ TOP DIAG BUFFER AND NEXT CROSS BUFFER
-      // TODO : FINISH STATE
+      // READ BUFFERED VALUES
+      when(previous_cross_buffers(previous_cross)(0) === 0.U){
+        cross_buffer(2) := 0.U
+        cross_buffer(4) := 0.U
+      }
+      when(bot_diag_buffers(previous_line)(buffer_idx)(1) === 0.U){
+        cross_buffer(2) := 0.U
+        cross_buffer(3) := 0.U
+      }
+      when(previous_cross_buffers(previous_cross)(1) === 0.U){
+        cross_buffer(4) := 0.U
+      }
+      when(bot_diag_buffers(previous_line)(buffer_idx)(0) === 0.U){
+        cross_buffer(2) := 0.U
+      }
+      when(top_actual_diag_buffer(buffer_idx + 1.U)(0) === 0.U){
+        cross_buffer(3) := 0.U
+      }
 
       // PIXEL BLACK : UPDATE CROSS BUFFER
       when(io.dataRead(7,0) === 0.U) {
@@ -294,11 +437,32 @@ class AcceleratorOpti3 {
         }
       }
 
-      switch(read_idx){
-        is(8.U){
-
+      //EXIT
+      when(read_idx === 12.U){
+        state := writeCross
+        read_idx := 0.U
+      }.elsewhen(!cross_addresses_valid(read_idx+1.U) || !outer_usefull(read_idx+1.U - 5.U) && read_idx < 11.U){
+        when(!cross_addresses_valid(read_idx+2.U) || !outer_usefull(read_idx+2.U - 5.U) && read_idx < 10.U){
+          when(!cross_addresses_valid(read_idx+3.U) || !outer_usefull(read_idx+3.U - 5.U) && read_idx < 9.U){
+            when(!cross_addresses_valid(read_idx+4.U) || !outer_usefull(read_idx+4.U - 5.U) && read_idx < 8.U){
+              state := writeCross
+              read_idx := 0.U
+            }.otherwise {
+              read_idx := read_idx + 4.U
+            }
+          }.otherwise{
+            read_idx := read_idx + 3.U
+          }
+        }.otherwise{
+          read_idx := read_idx + 2.U
         }
+      }.otherwise {
+        read_idx := read_idx + 1.U
       }
+    }
+
+    is(done){
+      io.done := true.B
     }
   }
 }
